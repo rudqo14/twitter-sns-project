c194541ca20b2c279b9562d9ecb9f5e4a3a96ba0
김경배 <keyoungbaekim@gimgyeongbaeui-MacBookPro.local>
2021-04-30 00:29:58 +0900

    Add: First commit, 초기 설정 적용

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  devtool: prod ? 'hidden-source-map' : 'eval',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          publicPath: '/some/path/',
+          postTransformPublicPath: (p) => `__webpack_public_path__ + ${p}`,
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port: 3000,
+    hot: true,
+    publicPath: '/',
+  },
+
+  plugins: [
+    new MiniCssExtractorPlugin({ filename: 'dist/style.css' }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
