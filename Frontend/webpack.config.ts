16e9be38ef2780066bfe4707d77e4f17bee04175
KeyoungBae Kim <rudqo14@gmail.com>
2021-05-03 07:49:36 +0900

    Add: babel 적용, html 파일명 변경

--- Frontend/webpack.config.ts
+++ Frontend/webpack.config.ts
@@ -21,7 +21,7 @@ const config: webpack.Configuration = {
     },
   },
   entry: {
-    app: './client',
+    app: './index',
   },
   module: {
     rules: [
@@ -58,7 +58,7 @@ const config: webpack.Configuration = {
     new ForkTsCheckerWebpackPlugin({
       async: false,
       // eslint: {
-      //   files: "./src/**/*",
+      //   files: './src/**/*',
       // },
     }),
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
